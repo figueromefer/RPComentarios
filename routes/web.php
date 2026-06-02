43116c24b52978c75bf50745ccb3434c415c712e
figueromefer <ffigueroa@boveda-creativa.com>
2026-06-02 10:41:07 -0600

    Agregar rutas para recetas de usuario

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\RecetaUsuarioController;
 use Illuminate\Support\Facades\Route;
 
 /*Route::get('/', function () {
@@ -14,6 +15,11 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
     Route::resource('comentarios', ComentarioController::class)->only(['index','edit','update','show']);
+
+    Route::get('recetas-usuario', [RecetaUsuarioController::class, 'index'])->name('recetas-usuario.index');
+    Route::get('recetas-usuario/{receta}/edit', [RecetaUsuarioController::class, 'edit'])->name('recetas-usuario.edit');
+    Route::put('recetas-usuario/{receta}', [RecetaUsuarioController::class, 'update'])->name('recetas-usuario.update');
+    Route::patch('recetas-usuario/{receta}/status', [RecetaUsuarioController::class, 'changeStatus'])->name('recetas-usuario.status');
 });
 
 Route::get('/dashboard', function () {
